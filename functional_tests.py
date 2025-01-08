945e4885399950f7fda234582f232c02d15ac3b1
daniilseliverstov <selivrstov91@internet.ru>
2025-01-08 20:24:47 +0300

    add app

--- functional_tests.py
+++ functional_tests.py
@@ -1,3 +1,19 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.edge()
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Edge()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start(self):
+        self.browser.get("http://localhost:8000")
+        self.assertIn("Congratulation", self.browser.title)
+        #self.fail("Finish the test!")
+
+
+if __name__ == "__main__":
+    unittest.main()
